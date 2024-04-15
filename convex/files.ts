3bd7b4c452109479afeddcb96ac40537edbd0346
MALTHE-PC\malth <malthe3@outlook.com>
2024-04-15 16:23:58 +0200

    Add dependencies and update schema for orgId in Convex

--- convex/files.ts
+++ convex/files.ts
@@ -4,29 +4,35 @@ import { mutation, query } from "./_generated/server";
 export const createFile = mutation({
   args: {
     name: v.string(),
+    orgId: v.string(),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) {
-      throw new ConvexError("You must be signed in to create a file.");
-    }
+
+    // if (!identity) {
+    //   throw new ConvexError("You must be signed in to create a file.");
+    // }
 
     await ctx.db.insert("files", {
       name: args.name,
+      orgId: args.orgId,
     });
   },
 });
 
 export const getFiles = query({
-  args: {},
+  args: { orgId: v.string() },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) {
-      throw new ConvexError("You must be signed in to create a file.");
-    }
+    // if (!identity) {
+    //   throw new ConvexError("You must be signed in to create a file.");
+    // }
 
-    return ctx.db.query("files").collect();
+    return ctx.db
+      .query("files")
+      .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId))
+      .collect();
   },
 });
