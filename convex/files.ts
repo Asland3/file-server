a1e67de209fa961aa8ddaa62c1b9a9b932888451
MALTHE-PC\malth <malthe3@outlook.com>
2024-04-15 23:02:12 +0200

    Refactor file creation and access control logic in Convex

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,21 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server";
+import { getUser } from "./users";
+
+async function hasAccessToOrg(
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string,
+  orgId: string
+) {
+  const user = await getUser(ctx, tokenIdentifier);
+
+  const hasAccess =
+    user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+
+  console.log(hasAccess);
+
+  return hasAccess;
+}
 
 export const createFile = mutation({
   args: {
@@ -9,10 +25,21 @@ export const createFile = mutation({
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
 
+    if (!identity) {
+      throw new ConvexError("You must be signed in to create a file.");
+    }
 
-    // if (!identity) {
-    //   throw new ConvexError("You must be signed in to create a file.");
-    // }
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    );
+
+    if (!hasAccess) {
+      throw new ConvexError(
+        "You must be a member of the organization to create a file."
+      );
+    }
 
     await ctx.db.insert("files", {
       name: args.name,
@@ -26,9 +53,19 @@ export const getFiles = query({
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
 
-    // if (!identity) {
-    //   throw new ConvexError("You must be signed in to create a file.");
-    // }
+    if (!identity) {
+      throw new ConvexError("You must be signed in to create a file.");
+    }
+
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    );
+
+    if (!hasAccess) {
+      return [];
+    }
 
     return ctx.db
       .query("files")
